5e4d7fa8f6e6c9f116ee0f7ebff0961a20f42dae
Yinan Liu <yinan97@outlook.com>
2021-12-24 12:38:01 +0800

    1224 remap prtsc to right option key

--- init.lua
+++ init.lua
@@ -22,6 +22,8 @@ hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "m", open("NeteaseMusic"))
 hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "j", open("IntelliJ IDEA"))
 hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "k", open("Enan Kanban"))
 hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "w", open("wechat"))
+hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "s", open("slack"))
+hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "p", open("preview"))
 -- hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "c", open("Visual Studio Code"))
 -- hs.hotkey.bind({"alt","ctrl","cmd","shift"}, "p", function() hs.eventtap.keyStroke({},"F3") end)
 
@@ -46,60 +48,9 @@ remap({'ctrl'}, 'l', pressFn('right'))
 remap({'ctrl', 'alt'}, 'l', pressFn('alt','right')) -- word-wise moving
 remap({'ctrl', 'alt'}, 'h', pressFn('alt','left'))
 
--- u i o
--- j k l
--- m , .
--- remap({'cmd','ctrl','alt',"shift"},'space', pressFn('0'))
--- remap({'cmd','ctrl','alt',"shift"},'u', pressFn('7'))
--- remap({'cmd','ctrl','alt',"shift"},'i', pressFn('8'))
--- remap({'cmd','ctrl','alt',"shift"},'o', pressFn('9'))
--- remap({'cmd','ctrl','alt',"shift"},'j', pressFn('4'))
--- remap({'cmd','ctrl','alt',"shift"},'k', pressFn('5'))
--- remap({'cmd','ctrl','alt',"shift"},'l', pressFn('6'))
--- remap({'cmd','ctrl','alt',"shift"},'m', pressFn('1'))
--- remap({'cmd','ctrl','alt',"shift"},',', pressFn('2'))
--- remap({'cmd','ctrl','alt',"shift"},'.', pressFn('3'))
-
--- Credit for this implementation goes to @arbelt and @jasoncodes 🙇⚡️😻
---
---   https://gist.github.com/arbelt/b91e1f38a0880afb316dd5b5732759f1
---   https://github.com/jasoncodes/dotfiles/blob/ac9f3ac/hammerspoon/control_escape.lua
-
--- send_escape = false
--- last_mods = {}
-
--- control_key_handler = function()
---   send_escape = false
--- end
-
--- control_key_timer = hs.timer.delayed.new(0.12, control_key_handler)
-
--- control_handler = function(evt)
---   local new_mods = evt:getFlags()
---   if last_mods["ctrl"] == new_mods["ctrl"] then
---     return false
---   end
---   if not last_mods["ctrl"] then
---     last_mods = new_mods
---     send_escape = true
---     control_key_timer:start()
---   else
---     if send_escape then
---       hs.eventtap.keyStroke({}, "ESCAPE")
---     end
---     last_mods = new_mods
---     control_key_timer:stop()
---   end
---   return false
--- end
-
--- control_tap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, control_handler)
--- control_tap:start()
-
--- other_handler = function(evt)
---   send_escape = false
---   return false
--- end
-
--- other_tap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, other_handler)
--- other_tap:start()
+-- from: https://github.com/hetima/hammerspoon-foundation_remapping
+-- remap printscreen key to right option key
+local FRemap = require('foundation_remapping')
+remapper = FRemap.new({vendorID=0x17ef, productID=0x6047})
+remapper:remap('PrintScreen', 'ralt')
+remapper:register()
